8438c53eff1b997ac2b9392740d9587b0a111ae8
MLStruckmann <magnus.struckmann@gmail.com>
2020-04-18 14:27:45 +0200

    Add dashboard header

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,1 +1,23 @@
-# This the main script that holds all components together
+#######
+# Objective: build a dashboard that displays investment data
+# in line charts and maps
+######
+
+# Perform imports here:
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import pandas as pd
+
+# Launch the application:
+app = dash.Dash()
+
+# Create a Dash layout that contains a Graph component:
+app.layout = html.Div([
+    html.H1('Stock Dashboard',style={'display':'inline-block', 'verticalAlign':'top', 'width':'30%'})
+])
+
+# Add the server clause:
+if __name__ == '__main__':
+    app.run_server()
