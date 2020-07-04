110ff2d169a325a15c9a82ac3d49e60c9b5315c2
MLStruckmann <magnus.struckmann@gmail.com>
2020-07-04 17:47:26 +0200

    Add data transformation to json

--- dashboard/data/download-stock-data.py
+++ dashboard/data/download-stock-data.py
@@ -1,21 +1,41 @@
 import pandas as pd
 import yfinance as yf
+import json
 
-filename = "usa.csv"
+stock_config = "test"
 
-df = pd.read_csv("/home/magnus/Projekte/grandmas-dashboard/dashboard/data/config_stocks/" + filename)
+df = pd.read_csv(f"/home/magnus/Projekte/grandmas-dashboard/dashboard/data/config_stocks/{stock_config}.csv")
 data = df["ticker"].to_list()
-print(type(data))
-print(data)
+
+# Define keys for stock reference data
+keys = ["longName","symbol","sector","industry","country","sharesOutstanding","longBusinessSummary","currency"]
+
+# Define start date for stock price data
+start_date = "2020-07-01"
 
 for stock in data:
-    print(f"Download {stock}")
-    stock_data = yf.download(stock, start="2020-06-15")
-    stock_data = stock_data[["Close"]]
-    print(stock_data)
-    print(type(stock_data))
-    print(stock_data.shape)
-    stock_data.to_csv(f"/home/magnus/Projekte/grandmas-dashboard/dashboard/data/downloads/{stock}.csv")
 
+    stock_dict = {}
 
+    # Download reference data
+    print(f"Download {stock} reference data")
+    stock_reference = {}
+    stock_info = yf.Ticker(stock)
+    stock_reference = stock_info.info
+    print(stock_reference)
+    for key in keys:
+        stock_dict[key] = stock_reference.get(key)
 
+    # Download stock data
+    print(f"Download {stock} stock data")
+    stock_price = []
+    stock_price = yf.download(stock, start=start_date)
+    stock_price = stock_price[["Close"]]
+    stock_price.index = stock_price.index.map(str)
+    stock_price.index = stock_price.index.map(lambda x: x[:10])
+    stock_dict["closingPrice"] = stock_price["Close"].to_dict()
+
+    # Save data in json format
+    file_name = f"/home/magnus/Projekte/grandmas-dashboard/dashboard/data/downloads/{stock_config}/{stock}.json"
+    with open(file_name, "w", encoding="utf8") as fp:
+        json.dump(stock_dict, fp, ensure_ascii=False)
